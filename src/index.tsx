29417c4d9096704a40751a8ad18b0debd3537fc5
oleksii_mialo <oleksii.mialo@headworks.com.ua>
2024-05-25 17:44:40 +0300

    migrate to sass

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './styles/index.scss';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-
 
 root.render(
   <React.StrictMode>
